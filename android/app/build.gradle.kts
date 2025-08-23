411e2ee7ab718297148b4ab06f0423d9c1e90afa
Stefan Mihajlovic <stefanmihajlovic012@gmail.com>
2025-08-23 16:32:53 +0200

    chore: align scaffold to io.kasseta/kasseta

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.kasseta_dev"
+    namespace = "io.kasseta_dev"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.kasseta_dev"
+        applicationId = "io.kasseta_dev"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
